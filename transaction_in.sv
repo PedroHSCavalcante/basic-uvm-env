89838d8f4dcf2e4a90f3258876a5779c78079c9c
Pedro Henrique Silva Cavalcante <pedro.cavalcante@embedded.ufcg.edu.br>
2019-07-26 08:30:20 -0300

    Add transaction_in.sv

---
+++ transaction_in.sv
@@ -0,0 +1,15 @@
+class transaction_in extends uvm_sequence_item;
+  rand bit data;
+
+  function new(string name = "");
+    super.new(name);
+  endfunction
+
+  `uvm_object_param_utils_begin(transaction_in)
+    `uvm_field_int(data, UVM_ALL_ON)
+  `uvm_object_utils_end
+
+  function string convert2string();
+    return $sformatf("{rdata = %h}",data;
+  endfunction
+endclass
