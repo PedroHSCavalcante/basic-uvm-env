1ae5c1fe19bc3e1dbd71f3155b57436d10dddfbd
Pedro Henrique Silva Cavalcante <pedro.cavalcante@embedded.ufcg.edu.br>
2019-07-29 11:10:46 -0300

    Updates sqrt model

--- refmod.cpp
+++ refmod.cpp
@@ -1,20 +1,6 @@
 #include <stdio.h>
+#include <math.h>
 
 extern "C" bit [7:0] sqrt(bit [7:0] x){
-  int d = 2;
-  int s = 4;
-  
-  if(s>x){
-    r = d/2; 
-  } 	
-  else{
-    d = d+2;
-    s = s+d+1;
-    while(s <= x){
-      d = d+2;
-      s = s+d+1; 
-    }
-  }
-  r = d/2
-  return r;
+  return floor(sqrt((x)));
 }
