13d69679f8429c1c61fe91159807453da2d4ef3a
Pedro Henrique Silva Cavalcante <pedro.cavalcante@embedded.ufcg.edu.br>
2019-09-24 17:06:52 -0300

    Record transaction on refmod

--- refmod.sv
+++ refmod.sv
@@ -9,7 +9,7 @@ class refmod extends uvm_component;
     uvm_analysis_imp #(transaction_in, refmod) in;
     uvm_analysis_port #(transaction_out) out;
     
-    event begin_refmodtask;
+    event begin_refmodtask, begin_record, end_record;
 
     function new(string name = "refmod", uvm_component parent);
         super.new(name, parent);
@@ -24,17 +24,36 @@ class refmod extends uvm_component;
     
     virtual task run_phase(uvm_phase phase);
         super.run_phase(phase);
+        fork
+            refmod_task();
+            record_tr();
+        join
+    endtask: run_phase
+
+    task refmod_task();
         forever begin
             @begin_refmodtask;
             tr_out = transaction_out::type_id::create("tr_out", this);
+            -> begin_record;
             tr_out.result = my_sqrt(tr_in.data);
+            #10;
+            -> end_record;
             out.write(tr_out);
         end
-    endtask: run_phase
+    endtask : refmod_task
 
     virtual function write (transaction_in t);
         tr_in = transaction_in#()::type_id::create("tr_in", this);
         tr_in.copy(t);
        -> begin_refmodtask;
     endfunction
+
+    virtual task record_tr();
+        forever begin
+            @(begin_record);
+            begin_tr(tr_out, "refmod");
+            @(end_record);
+            end_tr(tr_out);
+        end
+    endtask
 endclass: refmod
